6d2f660ecc645a2126fa5e5874ab16f3b104c1b3
William Rezende <williamrezendee@gmail.com>
2022-01-25 22:41:58 -0800

    Singleton pattern code example

---
+++ Design Patterns By Creation/Singleton Pattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador_1 = Singleton.GetInstancia;
+            jogador_1.Mensagem("Jogador 1: A bola está comigo no meio do campo.");
+
+            Singleton jogador_2 = Singleton.GetInstancia;
+            jogador_2.Mensagem("Jogador 2: Recebeu a bola.");
+
+            Singleton jogador_3 = Singleton.GetInstancia;
+            jogador_3.Mensagem("Jogador 3: Recebeu o lançamento na linha de fundo.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design Patterns By Creation/Singleton Pattern/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton instancia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Singleton();
+                    Console.WriteLine("Bola em Jogo!");
+                }
+                return instancia;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
